e1a3b768c3cae5fb4a94ab7062aba7506af9c9d0
pdh224 <qkrehdgmldkdlel@gmail.com>
2024-04-26 11:26:42 +0900

    163페이지까지

--- apps/app.py
+++ apps/app.py
@@ -4,10 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 from apps.config import config
+from flask_login import LoginManager
 
 db=SQLAlchemy()
 
 csrf=CSRFProtect()
+
+login_manager=LoginManager()
+
+login_manager.login_view="auth.signup"
+
+login_manager.login_message=""
+
 
 def create_app(config_key):
     app = Flask(__name__)
@@ -24,7 +32,13 @@ def create_app(config_key):
     csrf.init_app(app)
     db.init_app(app)
     Migrate(app, db)
+    login_manager.init_app(app)
 
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
+
+    from apps.auth import views as auth_views
+    app.register_blueprint(auth_views.auth, url_prefix="/auth")
+
+
     return app
--- apps/crud/views.py
+++ apps/crud/views.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template,redirect, url_for
 from apps.app import db
 from apps.crud.models import User
 from apps.crud.forms import UserForm
+from flask_login import login_required
 
 crud = Blueprint(
     "crud",
@@ -11,15 +12,18 @@ crud = Blueprint(
 )
 
 @crud.route("/")
+@login_required
 def index():
     return render_template("crud/index.html")
 
 @crud.route("/sql")
+@login_required
 def sql():
    
     return "콘솔 로그를 확인해 주세요"
 
 @crud.route("/users/new", methods=["GET","POST"])
+@login_required
 def create_user():
     form=UserForm()
     if form.validate_on_submit():
@@ -36,11 +40,13 @@ def create_user():
 
 
 @crud.route("/users")
+@login_required
 def users():
     users=User.query.all()
     return render_template("crud/index.html",users=users)
 
 @crud.route("/users/<user_id>", methods=["GET","POST"])
+@login_required
 def edit_user(user_id):
     form=UserForm()
     user=User.query.filter_by(id=user_id).first()
@@ -54,6 +60,7 @@ def edit_user(user_id):
     return render_template("crud/edit.html",user=user, form= form)
 
 @crud.route("/users/<user_id>/delete",methods=["POST"])
+@login_required
 def delete_user(user_id):
     user=User.query.filter_by(id=user_id).first()
     db.session.delete(user)
